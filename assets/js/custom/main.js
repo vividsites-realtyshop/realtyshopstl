4d2ef8f7c38003f706518409a2b2848c3008e005
Warren Kesselring <warren@thekestrel.co>
2021-09-17 10:17:02 -0500

    ...

--- assets/js/custom/main.js
+++ assets/js/custom/main.js
@@ -16,8 +16,11 @@ function setUpListings() {
         console.log("listingStart");
         listings[i].addEventListener("mouseover", function() {
             if (this.classList.contains("activated")) return null;
-            const fav = this.querySelector(".icon-fav");
-            if (fav) fav.dataset.address = this.getAttribute("aria-label");
+            const fav = this.querySelector(".icon-fav i");
+            if (fav) {
+                fav.dataset.action_label = "listing favorite";
+                fav.dataset.address = this.getAttribute("aria-label");
+            }
             this.classList.add("activated");
         });
         console.log("listingEnd");
--- main.js
+++ main.js
@@ -19,8 +19,13 @@ function setUpListings() {
     console.log("listingStart");
     listings[i].addEventListener("mouseover", function () {
       if (this.classList.contains("activated")) return null;
-      var fav = this.querySelector(".icon-fav");
-      if (fav) fav.dataset.address = this.getAttribute("aria-label");
+      var fav = this.querySelector(".icon-fav i");
+
+      if (fav) {
+        fav.dataset.action_label = "listing favorite";
+        fav.dataset.address = this.getAttribute("aria-label");
+      }
+
       this.classList.add("activated");
     });
     console.log("listingEnd");
